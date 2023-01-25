ffea61be1ebd6c9117aa8ae7ae4e431b6d73b8c9
m1theusr <m1theusrodrigues@gmail.com>
2023-01-25 11:15:31 -0300

    comunicação-back&front

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
-const app = require('./app');
+const database = require ('../src/db');
+const app = require('../src/app');
 
 app.listen(process.env.PORT, () => {
-    console.log('App is running');
+    console.log('App is running' + process.env.PORT);
 
 })
